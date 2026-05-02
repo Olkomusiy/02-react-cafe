10b539f10390e3d41a80a4e2b9e996a0bc523ffe
Oleh Musii <oleg.musiy321@gmail.com>
2026-05-02 12:42:02 +0300

    Update task

---
+++ src/components/VoteStats/VoteStats.tsx
@@ -0,0 +1,42 @@
+import css from './VoteStats.module.css';
+
+interface Votes {
+    good: number;
+    neutral: number;
+    bad: number;
+}
+
+interface VoteStatsProps {
+    votes: Votes;
+    totalVotes: number;
+    positiveRate: number;
+}
+
+const VoteStats = ({
+    votes,
+    totalVotes,
+    positiveRate,
+}: VoteStatsProps) => {
+    return (
+        <div className={css.container}>
+            <p className={css.stat}>
+                Good: <strong>{votes.good}</strong>
+            </p>
+            <p className={css.stat}>
+                Neutral: <strong>{votes.neutral}</strong>
+            </p>
+            <p className={css.stat}>
+                Bad: <strong>{votes.bad}</strong>
+            </p>
+            <p className={css.stat}>
+                Total: <strong>{totalVotes}</strong>
+            </p>
+            <p className={css.stat}>
+                Positive: <strong>{positiveRate}%</strong>
+            </p>
+
+        </div>
+    );
+};
+
+export default VoteStats;
